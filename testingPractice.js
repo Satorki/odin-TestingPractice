97da4109a1fc3011cfdd5d5dbb890a522fc60e63
satork <sebastian.kedzier@gmail.com>
2024-04-26 19:18:08 +0200

    test and function of capitalized and reversedString

---
+++ testingPractice.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function capitalized(str) {
+  return str.toUpperCase();
+}
+
+function reversedString(str) {
+  return str.split("").reverse().join("");
+}
+
+export { sum, capitalized, reversedString};
+
+//console.log(reversedString("string"));
